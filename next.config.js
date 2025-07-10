ba8aa66f52e80df2e01b9271b6d0e43db1f7ea34
Damita George <dg662@cornell.edu>
2025-07-09 23:27:02 -0700

    Configure base path and add static output directory

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: "/ds-geo.github.io",
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
